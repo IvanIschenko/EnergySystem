9bd8113ba4865d08c69eb1a1d4696bdd38cb9384
Ivan Ishchenko <ishchenko.io@yandex.ru>
2016-11-16 01:49:16 +0200

    New logic was added : - The fire will occur if we will add a lot of things to our electricity network.

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,17 @@ public class Main {
         Lamp lamp = new Lamp();
         Radio radio = new Radio();
 
-        // event subscribe
+        // event subscribe. One event - different subscribers
         sw.addElectricityListener(lamp);
         sw.addElectricityListener(radio);
+        sw.addElectricityListener(
+                new ElectricityConsumer() {
+                    @Override
+                    public void electricityOn() {
+                        System.out.println("Fire!");
+                    }
+                }
+        );
 
         sw.switchOn();
     }
